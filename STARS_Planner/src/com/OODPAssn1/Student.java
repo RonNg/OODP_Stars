74b6a7e159dfb217e3fbfec74609d89a474b01de
Ron Ng <ronngjianying@gmail.com>
2017-03-15 15:25:32 +0800

    Migrated NetBeans project to IntelliJ
    Signed-off-by: Ron Ng <ronngjianying@gmail.com>

--- STARS_Planner/src/com/OODPAssn1/Student.java
+++ STARS_Planner/src/com/OODPAssn1/Student.java
@@ -1,3 +1,5 @@
+package com.OODPAssn1;
+
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -17,7 +19,7 @@ public class Student implements Serializable
 {
     //SerialVersionUID of this Class used to deconflict serialisation
     static final long serialVersionUID = 1L;
-    
+
     //Basic Info
     private String name;
     private String matricNo;
@@ -25,44 +27,44 @@ public class Student implements Serializable
     private int contact;
     private String gender;
     private String nationality;
-  
+
     //Login Info
     private String username;
     private String password;
-    
+
     //Courses info
     private List<Integer> courseIndexList; //Stores the courses that this student is taking
-    
-    public Student (String name, String email, String matricNo, 
+
+    public Student (String name, String email, String matricNo,
                     int contact, String gender, String nationality,
                     String username, String password)
     {
         this.name = name; this.email = email; this.matricNo = matricNo;
         this.contact = contact; this.gender = gender; this.nationality = nationality;
-        
+
         //@TODO
         this.username = "default";
         this.password = "password";
     }
 
-   
-    
+
+
     //Accessors
     public String getName(){return this.name;}
     public String getMatricNo(){return this.matricNo;}
     public String getEmail(){return this.email;}
-   
+
     //Mutator
     public void addCourse(int courseIndex)
     {
-       if(courseIndexList == null)
-           courseIndexList = new ArrayList<>();
-       
-       courseIndexList.add(courseIndex);           
+        if(courseIndexList == null)
+            courseIndexList = new ArrayList<>();
+
+        courseIndexList.add(courseIndex);
     }
-    
-    
-    
-    
-    
+
+
+
+
+
 }
